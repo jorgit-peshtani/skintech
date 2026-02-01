93037911ea4a1ac2a2723134833b1ae2880e4ea9
Jorgit Peshtani <69903254+jorgit-peshtani@users.noreply.github.com>
2026-02-01 02:16:13 +0100

    Fix TypeError by defining Category model with exclude_from_menu field

--- backend_django/apps/catalogue/models.py
+++ backend_django/apps/catalogue/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from oscar.apps.catalogue.abstract_models import AbstractProduct as CoreAbstractProduct
+from oscar.apps.catalogue.abstract_models import AbstractCategory as CoreAbstractCategory
+
+from oscar.apps.catalogue.models import *
 
 class Product(CoreAbstractProduct):
     brand = models.CharField(max_length=100, blank=True, null=True)
 
-from oscar.apps.catalogue.models import * 
+class Category(CoreAbstractCategory):
+    exclude_from_menu = models.BooleanField(
+        default=False, 
+        db_index=True,
+        verbose_name="Exclude from menu",
+        help_text="Exclude this category from the menu."
+    )
+    long_description = models.TextField(blank=True, verbose_name="Long description") 
